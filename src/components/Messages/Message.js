9f45619a6a7301a3542279040f0b2a360b812170
Niloy Saha Roy <niloy.roy@northsouth.edu>
2019-03-27 13:47:37 +0600

    file upload done

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Comment } from "semantic-ui-react";
+import { Comment, Image } from "semantic-ui-react";
 import moment from "moment";
 
 const isOwnMessage = (message, user) => {
   return message.user.id === user.uid ? "messageSelf" : "";
 };
-
+const isImage = message => {
+  return message.hasOwnProperty("image") && !message.hasOwnProperty("content");
+};
 const timeForNow = timestamp => moment(timestamp).fromNow();
 const Message = ({ message, user }) => (
   <Comment>
@@ -13,7 +15,12 @@ const Message = ({ message, user }) => (
     <Comment.Content className={isOwnMessage(message, user)}>
       <Comment.Author as="a">{message.user.name}</Comment.Author>
       <Comment.Metadata>{timeForNow(message.timestamp)}</Comment.Metadata>
-      <Comment.Text>{message.content}</Comment.Text>
+
+      {isImage(message) ? (
+        <Image src={message.image} className="message__image" />
+      ) : (
+        <Comment.Text>{message.content}</Comment.Text>
+      )}
     </Comment.Content>
   </Comment>
 );
--- src/components/Messages/MessagesForm.js
+++ src/components/Messages/MessagesForm.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import firebase from "../../firebase";
+import uuidv4 from "uuid/v4";
 import { Segment, Input, Button } from "semantic-ui-react";
 
 import FileModal from "./FileModal";
 
 export default class MessagesForm extends Component {
   state = {
+    storageRef: firebase.storage().ref(),
+    uploadTask: null,
+    uploadState: "",
+    percentUploaded: 0,
     message: "",
     channel: this.props.currentChannel,
     user: this.props.currentUser,
@@ -20,16 +25,20 @@ export default class MessagesForm extends Component {
   changeHandeler = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
-  createMessage = () => {
+  createMessage = (fileurl = null) => {
     const message = {
       timestamp: firebase.database.ServerValue.TIMESTAMP,
       user: {
         id: this.state.user.uid,
         name: this.state.user.displayName,
         avatar: this.state.user.photoURL
-      },
-      content: this.state.message
+      }
     };
+    if (fileurl !== null) {
+      message["image"] = fileurl;
+    } else {
+      message["content"] = this.state.message;
+    }
     return message;
   };
   sendMessage = () => {
@@ -61,7 +70,69 @@ export default class MessagesForm extends Component {
   };
 
   uploadFile = (file, metadata) => {
-    console.log(file, metadata);
+    const pathToUpload = this.state.channel.id;
+    const ref = this.props.messagesRef;
+
+    const filepath = `chat/public/${uuidv4()}.jpg`;
+
+    this.setState(
+      {
+        uploadState: "uploading",
+        uploadTask: this.state.storageRef.child(filepath).put(file, metadata)
+      },
+      () => {
+        this.state.uploadTask.on(
+          "state_changed",
+          snap => {
+            const percentUploaded = Math.round(
+              (snap.bytesTransferred / snap.totalBytes) * 100
+            );
+            this.setState({ percentUploaded });
+          },
+          err => {
+            console.error(err);
+            this.setState({
+              errors: this.state.errors.concat(err),
+              uploadState: "error",
+              uploadTask: null
+            });
+          },
+          () => {
+            this.state.uploadTask.snapshot.ref
+              .getDownloadURL()
+              .then(downloadurl => {
+                this.sendFileMessage(downloadurl, ref, pathToUpload);
+              })
+              .catch(err => {
+                console.error(err);
+                this.setState({
+                  errors: this.state.errors.concat(err),
+                  uploadState: "error",
+                  uploadTask: null
+                });
+              });
+          }
+        );
+      }
+    );
+  };
+
+  sendFileMessage = (fileurl, ref, pathToUpload) => {
+    ref
+      .child(pathToUpload)
+      .push()
+      .set(this.createMessage(fileurl))
+      .then(() => {
+        this.setState({
+          uploadState: "done"
+        });
+      })
+      .catch(err => {
+        console.error(err);
+        this.setState({
+          errors: this.state.errors.concat(err)
+        });
+      });
   };
 
   render() {
