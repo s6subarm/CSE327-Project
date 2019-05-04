2ad665fa0ed3b8f21a3938884352b06e38b44965
SaykotRayhan <saykotrayhan199@gmail.com>
2019-05-05 01:14:13 +0600

    testing done

--- test/test.js
+++ test/test.js
@@ -1,24 +1,43 @@
-var assert = require('assert');
+var firebase = require("firebase");
+var assert = require("assert");
+var config = {
+  apiKey: "AIzaSyANAEQC2gM-2m98GQzaPHEVB9mRd0QZfdo",
+  authDomain: "react-slack-clone-be18f.firebaseapp.com",
+  databaseURL: "https://react-slack-clone-be18f.firebaseio.com",
+  projectId: "react-slack-clone-be18f",
+  storageBucket: "react-slack-clone-be18f.appspot.com",
+  messagingSenderId: "188608547096"
+};
 
-describe('Login', function(){
-    describe('submitHandler()', function(){
-        it('should succeesfully login a user with valid username and password', function(){
-            assert.equal(true/*login.submitHandler(email, password)*/, true);
+firebase.initializeApp(config);
+
+describe("Login", function() {
+  describe("submitHandler()", function() {
+    it("should succeesfully login a user with valid username and password", function() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword("rayhan@gmail.com", "123456")
+        .then(signedinUser => {
+          assert.equal(signedinUser.user.email, "rayhan@gmail.com");
         });
     });
+  });
 });
 
-describe('Login', function(){
-    describe('submitHandler()', function(){
-        it('should succeesfully deny a user to login with invalid password', function(){
-            assert.equal(login.submitHandler('rayhan@gmail.com', 'bhulPassword'), false);
+describe("Login", function() {
+  describe("submitHandler()", function() {
+    it("should succeesfully deny a user to login with invalid password", function() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword("muser@test1.net", "1231j23")
+        .then()
+        .catch(err => {
+          assert.ok(true);
         });
     });
+  });
 });
 
-
-
-
-// equal function takes two parameter. first one is a method call 
+// equal function takes two parameter. first one is a method call
 //and the second one is expected output from that particular call with specific values
 //assert will check if the input and output is correct or not.
